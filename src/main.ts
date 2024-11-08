a90a73bbee8ebc250c985c49e3196d0103110245
Ayla <76013118+aylaisnice@users.noreply.github.com>
2024-11-08 19:26:58 -0300

    why was that ther

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import {login} from './login.ts';
-import './navigation.ts';
+import { login } from "./login.ts";
+import "./navigation.ts";
 
 await login();
 
@@ -11,29 +11,21 @@ await login();
 rpc.call('com.atproto.repo.putRecord', {data: {record: record, collection: 'app.scc.profile',repo: sessionStorage.getItem('userdid'), rkey: 'self'}})*/
 
 const path = window.location.pathname;
-const location = path.split('/')[1];
-const script = document.createElement('script');
-script.type = 'module';
-script.id = 'pagescript';
+const location = path.split("/")[1];
+const script = document.createElement("script");
+script.type = "module";
+script.id = "pagescript";
 switch (location) {
-  case 'profile':
-    if (path.split('/')[4]) {
-      script.src = '/src/post.ts';
-      script.setAttribute('location', 'post');
+  case "profile":
+    if (path.split("/")[4]) {
+      script.src = "/src/post.ts";
+      script.setAttribute("location", "post");
     } else {
-      script.src = '/src/profile.ts';
-      script.setAttribute('location', 'profile');
+      script.src = "/src/profile.ts";
+      script.setAttribute("location", "profile");
     }
     break;
   default:
     break;
 }
 document.body.appendChild(script);
-
-document.addEventListener('DOMContentLoaded', function () {
-  const links = document.querySelectorAll('a');
-
-  links.forEach((link) => {
-    link;
-  });
-});
