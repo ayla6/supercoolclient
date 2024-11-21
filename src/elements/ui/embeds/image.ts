207b55f3aef34d19f04473d357cd33dd9adf7349
Ayla <76013118+aylaisnice@users.noreply.github.com>
2024-11-21 01:17:48 -0300

    image target fix

---
+++ src/elements/ui/embeds/image.ts
@@ -0,0 +1,78 @@
+import { AppBskyEmbedImages } from "@atcute/client/lexicons";
+import { elem } from "../../blocks/elem";
+
+const imageContainerSize = {
+  width: 500,
+  height: 500,
+};
+
+const forcePngFileTypes = ["webp", "gif"];
+export function image(
+  image: AppBskyEmbedImages.Image,
+  did: string,
+  numberOfImages: number = 1,
+) {
+  const ogsize = image.aspectRatio
+    ? {
+        width: image.aspectRatio.width,
+        height: image.aspectRatio.height,
+      }
+    : {
+        width: imageContainerSize.width * 2,
+        height: imageContainerSize.height * 2,
+      };
+  let width: number;
+  let height: number;
+
+  if (numberOfImages >= 2 && ogsize.width === ogsize.height) {
+    width = imageContainerSize.width / 2;
+    height = imageContainerSize.width / 2;
+  } else {
+    const ratio = ogsize.width / ogsize.height;
+    width = Math.min(
+      imageContainerSize.width,
+      imageContainerSize.height * ratio,
+    );
+    height = width / ratio;
+  }
+
+  const ogFileType = image.image.mimeType.split("/")[1];
+  const fullFileType = forcePngFileTypes.includes(ogFileType)
+    ? "png"
+    : ogFileType;
+
+  let thumbFileType = "webp";
+  let thumbSize = "thumbnail";
+  let imageRendering = "";
+  if (ogsize.width <= 1000 && ogsize.height <= 1000) {
+    thumbSize = "fullsize";
+    thumbFileType = fullFileType;
+    if (
+      ogsize.width <= imageContainerSize.width &&
+      ogsize.height <= imageContainerSize.height
+    )
+      imageRendering = "pixelated";
+  }
+
+  return elem(
+    "a",
+    {
+      className: "image",
+      href: `https://cdn.bsky.app/img/feed_fullsize/plain/${did}/${image.image.ref.$link}@${fullFileType}`,
+      target: " ",
+    },
+    [
+      elem("img", {
+        src: `https://cdn.bsky.app/img/feed_${thumbSize}/plain/${did}/${image.image.ref.$link}@${thumbFileType}`,
+        title: image.alt,
+        alt: image.alt,
+        loading: "lazy",
+        width: width,
+        height: height,
+        style: {
+          imageRendering: imageRendering,
+        } as CSSStyleDeclaration,
+      }),
+    ],
+  );
+}
