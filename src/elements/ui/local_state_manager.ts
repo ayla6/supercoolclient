11643ffffea16ae824eb9d67e7638a8677427763
Ayla <76013118+aylaisnice@users.noreply.github.com>
2025-02-09 17:11:06 -0300

    dumbass

--- src/elements/ui/local_state_manager.ts
+++ src/elements/ui/local_state_manager.ts
@@ -20,6 +20,7 @@ export const createFeedManager = (
   home: boolean = false,
 ) => {
   const path = window.location.pathname;
+  feedsData = feedsData.filter(Boolean);
 
   const setHomeNavButton = (feed: Feed) => {
     (
